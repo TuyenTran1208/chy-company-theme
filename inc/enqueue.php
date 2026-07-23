eac57dfcab2cb9d94d4caea337331b9ba39187f6
Tuyn128 <tranthanhtuyen9a1tqk@gmail.com>
2026-07-23 14:19:15 +0700

    fix(theme): enqueue modular css assets

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -17,9 +17,44 @@ if ( ! defined( 'ABSPATH' ) ) {
 function chy_company_theme_enqueue_assets() {
 
 	wp_enqueue_style(
-		'chy-company-theme-style',
-		get_template_directory_uri() . '/assets/css/theme.css',
+		'chy-company-theme-base',
+		get_template_directory_uri() . '/assets/css/base.css',
 		array(),
+		CHY_COMPANY_THEME_VERSION
+	);
+
+	wp_enqueue_style(
+		'chy-company-theme-layout',
+		get_template_directory_uri() . '/assets/css/layout.css',
+		array( 'chy-company-theme-base' ),
+		CHY_COMPANY_THEME_VERSION
+	);
+
+	wp_enqueue_style(
+		'chy-company-theme-components',
+		get_template_directory_uri() . '/assets/css/components.css',
+		array( 'chy-company-theme-layout' ),
+		CHY_COMPANY_THEME_VERSION
+	);
+
+	wp_enqueue_style(
+		'chy-company-theme-utilities',
+		get_template_directory_uri() . '/assets/css/utilities.css',
+		array( 'chy-company-theme-components' ),
+		CHY_COMPANY_THEME_VERSION
+	);
+
+	wp_enqueue_style(
+		'chy-company-theme-responsive',
+		get_template_directory_uri() . '/assets/css/responsive.css',
+		array( 'chy-company-theme-utilities' ),
+		CHY_COMPANY_THEME_VERSION
+	);
+
+	wp_enqueue_style(
+		'chy-company-theme-style',
+		get_stylesheet_uri(),
+		array( 'chy-company-theme-responsive' ),
 		CHY_COMPANY_THEME_VERSION
 	);
 
